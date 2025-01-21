c36f28ced73650b04f56afcb89c97cdde14662e0
dta0one <github@davidtao.net>
2025-01-21 08:31:18 -0800

    added char count

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def main():
     book_to_read = "books/frankenstein.txt"
     book_text = get_book_text(book_to_read)
     book_word_count = get_word_count(book_text)
+    book_char_dict = count_characters(book_text)
+    
 
-    print (book_text)
+    #print (book_text)
     print(f"word count: {book_word_count}")
+    print(f"character count: {book_char_dict}")
 
 def get_book_text(book):
     with open(book) as f:
@@ -13,6 +16,24 @@ def get_book_text(book):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    lowered_characters = text.lower()
+    characters_count = {}
+
+    for char in lowered_characters:
+        if char in characters_count:
+            characters_count[char] += 1
+        else:
+            characters_count[char] = 1
+    return characters_count
+    
+
+
+    #lowered_characters = text.lower()
+    #character_split = lowered_characters.split()
+    #return len(lowered_characters)
+
 
 main()
 
